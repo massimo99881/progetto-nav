6218e280a9ec15d8626f1f495e62cd6156419b52
jacopogirolamini2005 <jacopogirolamini2005@gmail.com>
2024-03-17 10:58:11 +0100

    update 1

---
+++ Nav/src/it/jacopo/nave/Update.java
@@ -0,0 +1,27 @@
+package it.jacopo.nave;
+
+public class Update extends Thread{
+	GameObject nav;
+	Panello pan;
+	public Update(GameObject nav, Panello pan) {
+		this.nav = nav;
+		this.pan = pan;
+	}
+	@Override
+	public void run() {
+		while(true) {
+			if(pan.cx-pan.obj.get("ciao").x < 20 && pan.cx-pan.obj.get("ciao").x > -20 && pan.cy-pan.obj.get("ciao").y < 20 && pan.cy-pan.obj.get("ciao").y > -20) { //se il cursore è vicino ferma 
+				pan.obj.get("ciao").speed = Double.MIN_VALUE;
+			}
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				pan.repaint();
+			
+		}
+		//pan.ThLavora = false;
+	}
+}
